4c5ba89f4b66c08673f205e2f1e28a2998a3ad87
Mia Contreras <Miacontreras@lewisu.edu>
2023-10-23 03:04:30 -0500

    final lite

--- map-mania.js
+++ map-mania.js
@@ -4,8 +4,37 @@ var gMap;
 function initMap() {
 gMap = new google.maps.Map(document.getElementById('myMapID'),{
     center: {lat: 41.878, lng:10}, zoom: 3});
+
+var marker = new google.maps.Marker({position:{lat:38.7223,lng:-9.1393}, map:gMap});
+
+var marker = new google.maps.Marker({position:{lat:41.9028,lng:12.4964}, map:gMap});
+
+var marker = new google.maps.Marker({position:{lat:42.5063,lng:1.5218}, map:gMap});
+
+var marker = new google.maps.Marker({position:{lat:48.8566,lng:2.3522}, map:gMap});
+
+var marker = new google.maps.Marker({position:{lat:51.5072,lng:0.1276}, map:gMap});
+
+var marker = new google.maps.Marker({position:{lat:21.1619,lng:-86.8515}, map:gMap});
+
+var marker = new google.maps.Marker({position:{lat:22.8948,lng:-109.9152}, map:gMap});
+
+
 }
 
-function intApplication(){
+google.maps.event.addListener(gMap, 'idle', function(){
+    updateGame()
+    console.log('function updateGame()!');
+    var zoomLevel = gMap.getZoom()
+    var inBounds = false;
+
+    if (gMap.getBounds().contains({lat:38.7223,lng:-9.1393}))
+    inBounds = true;
+});
+
+console.log("inBounds:"+inBoubds+"zoomLevel:"+zoolLevel);
+
+
+function initApplication(){
     console.log('Map Mania Lite - Starting!');
 }
