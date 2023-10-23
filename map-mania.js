3af71f4364ea49f708c43e6de01bd8daa995a013
Mia Contreras <Miacontreras@lewisu.edu>
2023-10-23 01:20:32 -0500

    feature 3 change

--- map-mania.js
+++ map-mania.js
@@ -1,3 +1,11 @@
+
+var gMap;
+  
+function initMap() {
+gMap = new google.maps.Map(document.getElementById('myMapID'),{
+    center: {lat: 41.878, lng:10}, zoom: 3});
+}
+
 function intApplication(){
     console.log('Map Mania Lite - Starting!');
 }
